3e56bad8de055eb3fa176ce19d961a6ed860c197
Lucilio Correia <lucilio@lucilio.net>
2024-11-08 14:08:17 -0300

    add: theme templates

---
+++ patterns/footer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Title: footer
+ * Slug: carta-campinas/footer
+ * Inserter: no
+ */
+?>
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"width":"100%","style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"backgroundColor":"highlight","textColor":"base"} -->
+<div class="wp-block-column has-base-color has-highlight-background-color has-text-color has-background has-link-color" style="flex-basis:100%"><!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center"><?php esc_html_e('Carta Campinas é uma plataforma colaborativa de jornalismo. ', 'carta-campinas');?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center"><?php esc_html_e('O envio de conteúdo para publicação implica cessão de todos os direitos. ', 'carta-campinas');?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center"><?php esc_html_e('O material publicado na Carta Campinas pode ser reproduzido na web com crédito e link.', 'carta-campinas');?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:image {"sizeSlug":"large","linkDestination":"none","align":"center"} -->
+<figure class="wp-block-image aligncenter size-large"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/cc-by.svg" alt="" class=""/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
