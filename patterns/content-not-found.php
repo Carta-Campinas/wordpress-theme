98a3f77c20540318721c93a006630256d6515190
Lucilio Correia <lucilio@lucilio.net>
2024-12-11 20:26:47 -0300

    templates/404.html
    fix: refactor not found page using new news-item

--- patterns/content-not-found.php
+++ patterns/content-not-found.php
@@ -5,30 +5,147 @@
  * Inserter: no
  */
 ?>
-<!-- wp:columns {"style":{"spacing":{"margin":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}}} -->
-<div class="wp-block-columns" style="margin-top:var(--wp--preset--spacing--50);margin-bottom:var(--wp--preset--spacing--50)"><!-- wp:column {"width":"25%"} -->
-<div class="wp-block-column" style="flex-basis:25%"></div>
-<!-- /wp:column -->
+<!-- wp:columns {
+    "style":{
+        "spacing":{
+            "margin":{
+                "top":"var:preset|spacing|50",
+                "bottom":"var:preset|spacing|50"
+            }
+        }
+    }
+} -->
+<div 
+    class="wp-block-columns"
+    style="
+        margin-top:var(--wp--preset--spacing--50);
+        margin-bottom:var(--wp--preset--spacing--50)
+    "
+>
+    <!-- wp:column {"width":"25%"} -->
+    <div class="wp-block-column" style="flex-basis:25%">
+    </div>
+    <!-- /wp:column -->
+    <!-- wp:column {"width":"50%"} -->
+    <div class="wp-block-column" style="flex-basis:50%">
+        <!-- wp:group {
+            "style":{
+                "spacing":{
+                    "margin":{
+                        "top":"var:preset|spacing|30",
+                        "bottom":"var:preset|spacing|30"
+                    }
+                }
+            },
+            "layout":{
+                "type":"flex",
+                "flexWrap":"nowrap",
+                "justifyContent":"space-between",
+                "verticalAlignment":"center"
+            }
+        } -->
+        <div
+            class="wp-block-group"
+            style="
+                margin-top:var(--wp--preset--spacing--30);
+                margin-bottom:var(--wp--preset--spacing--30)
+            "
+        >
+            <!-- wp:heading {"level":1,"fontSize":"extra-large"} -->
+            <h1 class="wp-block-heading has-extra-large-font-size">
+                <?php esc_html_e('Not Found', 'carta-campinas');?>
+            </h1>
+            <!-- /wp:heading -->
+        </div>
+        <!-- /wp:group -->
+        <!-- wp:separator {
+            "className":"is-style-wide",
+            "style":{
+                "spacing":{
+                    "margin":{
+                        "top":"var:preset|spacing|50",
+                        "bottom":"var:preset|spacing|50"
+                    }
+                }
+            },
+            "backgroundColor":"highlight"
+        } -->
+        <hr
+            class="
+                wp-block-separator
+                has-text-color
+                has-highlight-color
+                has-alpha-channel-opacity
+                has-highlight-background-color
+                has-background
+                is-style-wide
+            "
+            style="
+                margin-top:var(--wp--preset--spacing--50);
+                margin-bottom:var(--wp--preset--spacing--50)
+            "
+        />
+        <!-- /wp:separator -->
+        <!-- wp:paragraph {
+            "style":{
+                "spacing":{
+                    "margin":{
+                        "bottom":"var:preset|spacing|60"
+                    }
+                }
+            },
+            "fontSize":"large"
+        } -->
+        <p
+            class="has-large-font-size"
+            style="
+                margin-bottom:var(--wp--preset--spacing--60)
+            "
+        >
+            <?php
+            esc_html_e(
+                'O conteúdo solicitado não pode ser encontrado.',
+                'carta-campinas'
+            );
+            ?>
+        </p>
+        <!-- /wp:paragraph -->
+        <!-- wp:paragraph {
+            "style":{
+                "spacing":{
+                    "margin":{
+                        "bottom":"var:preset|spacing|60"
+                    }
+                }
+            },
+            "fontSize":"large"
+        } -->
+        <p
+            class="has-large-font-size"
+            style="
+                margin-bottom:var(--wp--preset--spacing--60)
+            "
+        >
+            <?php
+            esc_html_e(
+                'Está a procura de algo específico? Use a nossa busca:',
+                'carta-campinas'
+            );
+            ?>
+        </p>
+        <!-- /wp:paragraph -->
+        <!-- wp:search {
+            "label":"Pesquisar",
+            "buttonText":"Pesquisar",
+            "buttonPosition":"button-inside",
+            "buttonUseIcon":true,
+            "backgroundColor":"highlight"
+        } /-->
+    </div>
+    <!-- /wp:column -->
 
-<!-- wp:column {"width":"50%"} -->
-<div class="wp-block-column" style="flex-basis:50%"><!-- wp:group {"style":{"spacing":{"margin":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between","verticalAlignment":"center"}} -->
-<div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--30);margin-bottom:var(--wp--preset--spacing--30)"><!-- wp:heading {"level":1,"fontSize":"extra-large"} -->
-<h1 class="wp-block-heading has-extra-large-font-size"><?php esc_html_e('Não Encontrado', 'carta-campinas');?></h1>
-<!-- /wp:heading --></div>
-<!-- /wp:group -->
-
-<!-- wp:separator {"className":"is-style-wide","style":{"spacing":{"margin":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"backgroundColor":"highlight"} -->
-<hr class="wp-block-separator has-text-color has-highlight-color has-alpha-channel-opacity has-highlight-background-color has-background is-style-wide" style="margin-top:var(--wp--preset--spacing--50);margin-bottom:var(--wp--preset--spacing--50)"/>
-<!-- /wp:separator -->
-
-<!-- wp:paragraph {"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|60"}}},"fontSize":"large"} -->
-<p class="has-large-font-size" style="margin-bottom:var(--wp--preset--spacing--60)"><?php esc_html_e('O conteúdo solicitado não pode ser encontrado. Está a procura de algo específico? Use a nossa busca:', 'carta-campinas');?></p>
-<!-- /wp:paragraph -->
-
-<!-- wp:search {"label":"Pesquisar","buttonText":"Pesquisar","buttonPosition":"button-inside","buttonUseIcon":true,"backgroundColor":"highlight"} /--></div>
-<!-- /wp:column -->
-
-<!-- wp:column {"width":"25%"} -->
-<div class="wp-block-column" style="flex-basis:25%"></div>
-<!-- /wp:column --></div>
+    <!-- wp:column {"width":"25%"} -->
+    <div class="wp-block-column" style="flex-basis:25%"></div>
+    <!-- /wp:column -->
+</div>
 <!-- /wp:columns -->
