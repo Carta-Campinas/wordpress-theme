f019ce864f9ef0c1d062ee654bf660081249cc03
Lucilio Correia <lucilio@lucilio.net>
2025-01-05 22:54:16 -0300

    fix: re-add translateable strings for single meta box

---
+++ patterns/single-meta-author-label.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Title: Rótulo autor (por)
+ * Slug: carta-campinas/single-meta-author-label
+ * Inserter: no
+ */
+?>
+<!-- wp:paragraph -->
+<p>
+    <?php esc_html_e('por ', 'carta-campinas');?>
+</p>
+<!-- /wp:paragraph -->
---
+++ patterns/single-meta-date-label.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Title: Rótulo data (em)
+ * Slug: carta-campinas/single-meta-date-label
+ * Inserter: no
+ */
+?>
+<!-- wp:paragraph -->
+<p>
+    <?php esc_html_e('em ', 'carta-campinas');?>
+</p>
+<!-- /wp:paragraph -->
