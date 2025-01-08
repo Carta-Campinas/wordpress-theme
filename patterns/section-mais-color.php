3092b8eca3da3e601bc159efd59bcf8de81ef713
Lucilio Correia <lucilio@lucilio.net>
2025-01-08 15:48:46 -0300

    fix: use advanced-query-loop plugin on yellow section

--- patterns/section-mais-color.php
+++ patterns/section-mais-color.php
@@ -4,60 +4,62 @@
  * Slug: carta-campinas/section-mais-color
  * Categories: News
  */
+
 ?>
 <!-- wp:group {
     "tagName":"section",
     "metadata":{
         "categories":["News"],
         "patternName":"carta-campinas/section-mais-color",
-        "name":"Section Saber & Saúde (color)"
+        "name":"Section Saber \u0026 Saúde (color)"
     },
-    "className":"section-mais",
-    "backgroundColor":"yellow",
-    "layout":{
-        "type":"constrained"
-    },
-    "style": {
+    "className":"section-mais wide",
+    "style":{
         "spacing":{
             "padding":"var(--wp--custom--news-card--radius)"
         },
         "border":{
             "radius":{
-                "topLeft": "var(--wp--custom--news-card--radius)",
-                "topRight": "var(--wp--custom--news-card--radius)"
+                "topLeft":"var(--wp--custom--news-card--radius)",
+                "topRight":"var(--wp--custom--news-card--radius)"
             }
         }
+    },
+    "backgroundColor":"yellow",
+    "layout":{
+        "type":"constrained"
     }
 } -->
-<section
+<section 
     class="
         wp-block-group
         section-mais
         wide
-        has-background
         has-yellow-background-color
+        has-background
     "
     style="
-        padding: var(--wp--custom--news-card--radius);
-        border-top-left-radius: var(--wp--custom--news-card--radius);
-        border-top-right-radius: var(--wp--custom--news-card--radius);
+        border-top-left-radius:var(--wp--custom--news-card--radius);
+        border-top-right-radius:var(--wp--custom--news-card--radius);
+        padding:var(--wp--custom--news-card--radius)
     "
-    >
-
-
-    <!-- wp:heading {
-        "level":2,
-        "textColor": "highlight",
-        "style":{
-            "spacing":{
-                "margin":{
-                    "bottom":"var:preset|spacing|50"
-                }
+>
+<!-- wp:heading {
+    "style":{
+        "spacing":{
+            "margin":{
+                "bottom":"var:preset|spacing|50"
             }
         }
-    } -->
+    },
+    "textColor":"highlight"
+} -->
     <h2
-        class="wp-block-heading has-text-color has-highlight-color"
+        class="
+            wp-block-heading
+            has-highlight-color
+            has-text-color
+        "
         style="margin-bottom:var(--wp--preset--spacing--50)"
     >
         <?php esc_html_e('Muito Mais', 'carta-campinas');?>
@@ -65,11 +67,11 @@
     <!-- /wp:heading -->
 
     <!-- wp:query {
-        "queryId":55,
+        "queryId":14,
         "query":{
             "perPage":3,
             "pages":0,
-            "offset":"0",
+            "offset":0,
             "postType":"post",
             "order":"desc",
             "orderBy":"date",
@@ -78,48 +80,46 @@
             "exclude":[],
             "sticky":"exclude",
             "inherit":false,
-            "taxQuery": {
-                "category_exclude": [<?php
-                echo implode(
-                    ', ',
-                    array_map(
-                        function ($item) {
-                            return $item->term_id;
-                        },
-                        get_categories(
-                            [
-                                'slug' => [
-                                    'politica',
-                                    'economia',
-                                    'economia-3',
-                                    'glaucocortez'
-                                ]
-                            ]
-                        )
-                    )
-                );?>]
+            "tax_query":{
+                "relation":"OR",
+                "queries":[
+                    {
+                        "id":"58a55207-40a9-43ec-994a-a685f90b5cf0",
+                        "taxonomy":"category",
+                        "terms":[
+                            "Artes",
+                            "Cultura",
+                            "Cultura SP",
+                            "Política",
+                            "Economia",
+                            "Economia e Política",
+                            "Manchete"
+                        ],
+                        "include_children":true,
+                        "operator":"NOT IN"
+                    }
+                ]
             }
         },
+        "namespace":"advanced-query-loop",
         "metadata":{
-            "categories":["news"],
-            "patternName":"core/block/129209",
-            "name":"News List"
-        },
-        "className":"news-list caption-wide caption-bottom"
+            "categories":["posts"],
+            "patternName":"core/query-grid-posts",
+            "name":"Grade"
+        }
     } -->
-    <div class="wp-block-query news-list caption-wide caption-bottom">
-        <!-- wp:post-template {
-            "layout":{
-                "type":"grid",
-                "columnCount":3,
-                "minimumColumnWidth":null
-            }
-        } -->
+    <div class="wp-block-query">
 
-        <!-- wp:pattern {"slug":"carta-campinas/news-item-cover-image-4-3-h5"} /-->
+        <!-- wp:post-template {"layout":{"type":"grid","columnCount":3}} -->
+
+            <!-- wp:pattern {
+                "slug":"carta-campinas/news-item-cover-image-4-3-h5"
+            } /-->
 
         <!-- /wp:post-template -->
+        
     </div>
     <!-- /wp:query -->
+
 </section>
 <!-- /wp:group -->
